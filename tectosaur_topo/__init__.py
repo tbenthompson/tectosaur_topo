fe69c8bb261e2793b36b66dacf6f5ccc865c5959
Ben Thompson <t.ben.thompson@gmail.com>
2017-10-22 22:18:41 -0400

    fix __init__.py

--- tectosaur_topo/__init__.py
+++ tectosaur_topo/__init__.py
@@ -1,4 +1,4 @@
 from tectosaur.util.logging import setup_root_logger
 logger = setup_root_logger(__name__)
 
-from tectosaur_topo.interface import solve_topo, interior_evaluate
+from tectosaur_topo.interface import solve_topo, evaluate_interior
